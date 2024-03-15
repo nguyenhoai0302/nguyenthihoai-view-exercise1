93480e1a686a3587546982e3608bf1e95dd09070
Hoai <hoai.nguyen25@student.passerellesnumeriques.org>
2024-03-15 23:52:43 +0700

    View

--- View/routes/web.php
+++ View/routes/web.php
@@ -16,3 +16,6 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('home');
 })->name('home');
+Route::get('/form', function () {
+    return view('taskList');
+})->name('taskList');
